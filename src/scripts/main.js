ffd1b863da80bd9c11541c315943cea381aec148
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-03-31 12:58:57 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.body;
+const successPhrase = '<div class="message">Promise was resolved!</div>';
+const errorPhrase =
+  '<div class="message error-message">Promise was rejected!</div>';
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => resolve());
+});
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Fatal error')), 3000);
+});
+
+promise1
+  .then(() => {
+    body.insertAdjacentHTML('beforeend', successPhrase);
+  })
+  .catch((error) => alert(error));
+
+promise2.catch(() => {
+  body.insertAdjacentHTML('beforeend', errorPhrase);
+});
